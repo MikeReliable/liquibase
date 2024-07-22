08e4e12883f1744ff4fd4b14f6566e7df1ae3af4
MikeReliable <nadjozhkin@gmail.com>
2024-07-22 14:21:07 +0700

    third commit. sortASC

--- src/main/java/ru/mike/liquibase/controller/LiquibaseController.java
+++ src/main/java/ru/mike/liquibase/controller/LiquibaseController.java
@@ -5,17 +5,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import ru.mike.liquibase.domain.Passenger;
 import ru.mike.liquibase.repo.PassengersRepo;
 
-import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class LiquibaseController {
@@ -26,18 +26,17 @@ public class LiquibaseController {
     @GetMapping(path = "list")
     public String main(@ModelAttribute Passenger passenger,
                        @RequestParam(value = "pageSize", defaultValue = "50") Integer pageSize,
-                       @RequestParam(value = "pageNumber",defaultValue = "0") Integer pageNumber,
-//                       @PageableDefault(sort = {"id"}, direction = Sort.Direction.ASC, size = 50) Pageable pageable,
+                       @RequestParam(value = "pageNumber", defaultValue = "0") Integer pageNumber,
+                       @RequestParam(value = "pageSort", defaultValue = "id") String pageSort,
                        Model model) {
-        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by("id"));
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(pageSort));
         Page<Passenger> passengerList = passengersRepo.findAll(pageable);
         int totalPages = passengerList.getTotalPages();
-//        int pageNumber = pageable.getPageNumber();
-
         model.addAttribute("passengerList", passengerList);
         model.addAttribute("totalPages", totalPages);
         model.addAttribute("pageNumber", pageNumber);
         model.addAttribute("pageSize", pageSize);
+        model.addAttribute("pageSort", pageSort);
         return "index";
     }
 
