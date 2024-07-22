119f084ab82000886afb62b0624bbca3c775f891
MikeReliable <nadjozhkin@gmail.com>
2024-07-22 15:34:30 +0700

    fourth commit. sortASC/DESC

--- src/main/java/ru/mike/liquibase/controller/LiquibaseController.java
+++ src/main/java/ru/mike/liquibase/controller/LiquibaseController.java
@@ -10,12 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import ru.mike.liquibase.domain.Passenger;
 import ru.mike.liquibase.repo.PassengersRepo;
-
-import java.util.Map;
-import java.util.Optional;
 
 @Controller
 public class LiquibaseController {
@@ -28,15 +24,23 @@ public class LiquibaseController {
                        @RequestParam(value = "pageSize", defaultValue = "50") Integer pageSize,
                        @RequestParam(value = "pageNumber", defaultValue = "0") Integer pageNumber,
                        @RequestParam(value = "pageSort", defaultValue = "id") String pageSort,
+                       @RequestParam(value = "direction", defaultValue = "ASC") String direction,
                        Model model) {
-        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(pageSort));
-        Page<Passenger> passengerList = passengersRepo.findAll(pageable);
+        Page<Passenger> passengerList;
+        if (direction.equals("ASC")) {
+            Pageable pageableASC = PageRequest.of(pageNumber, pageSize, Sort.by(Sort.Direction.ASC, pageSort));
+            passengerList = passengersRepo.findAll(pageableASC);
+        } else {
+            Pageable pageableDESC = PageRequest.of(pageNumber, pageSize, Sort.by(Sort.Direction.DESC, pageSort));
+            passengerList = passengersRepo.findAll(pageableDESC);
+        }
         int totalPages = passengerList.getTotalPages();
         model.addAttribute("passengerList", passengerList);
         model.addAttribute("totalPages", totalPages);
         model.addAttribute("pageNumber", pageNumber);
         model.addAttribute("pageSize", pageSize);
         model.addAttribute("pageSort", pageSort);
+        model.addAttribute("direction", direction);
         return "index";
     }
 
